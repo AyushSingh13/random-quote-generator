939aede76dab8ef5a2d0f3e4e2cae8f7781db3c3
Ayush Singh <ayush.vijay@gmail.com>
2016-12-30 17:43:20 +0000

    Added tweet button

--- quotes.js
+++ quotes.js
@@ -4,13 +4,24 @@
 */
 
 $(document).ready(function() {
-    $("#getQuote").on("click", function() {
-        $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json) {
-            var html = "";
-            html += "<div class='quoteMessage'>" + json.quoteText + "</div><br>";
-            html += "<div class='quoteAuthor'> - " + json.quoteAuthor + "</div><br>";
-            $(".quote").html(JSON.stringify(json));
-            $(".quote").html(html);
-        });
+    populateQuote();
+    $("#newQuote").on("click", function() {
+        populateQuote();
     });
 });
+
+function populateQuote() {
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json) {
+        var html = "";
+        html += "<div class='quoteMessage'>" + json.quoteText + "</div><br>";
+        html += "<div class='quoteAuthor'> - " + json.quoteAuthor + "</div><br>";
+        setTweetButton(json.quoteText, json.quoteAuthor);
+        $(".quote").html(html);
+    });
+}
+
+function setTweetButton(quote, author) {
+    $("#twitter-share-button").attr('href', 'https://twitter.com/intent/tweet?text="' + encodeURIComponent(
+        quote + '" - ' + author
+    ));
+}
